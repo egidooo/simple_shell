9986c69447aa302f1c912cbfc6db1f93ae564723
egidooo <kwizeraegide250@gmail.com>
2023-04-27 11:09:13 -0700

    progress

--- execmd.c
+++ execmd.c
@@ -1,19 +1,21 @@
 #include "shell.h"
 /**
-* execmd - execute the program reffered to pathname
-* @argv: argument vector
-* Return: void
-*/
+ * execmd - checks if the argv is not empty
+ * @argv: argument
+ * Return: void
+ */
+
 void execmd(char **argv)
 {
-char *command = NULL, *real_command = NULL;
-if (argv)
-{
-command = argv[0];
-real_command = get_location(command);
-if (execve(real_command, argv, NULL) == -1)
-{
-perror("error:");
-}
-}
+	char *command = NULL, *real_command = NULL;
+
+	if (argv)
+	{
+		command = argv[0];
+		real_command = get_location(command);
+		if (execve(real_command, argv, NULL) == -1)
+		{
+			perror("error");
+		}
+	}
 }
